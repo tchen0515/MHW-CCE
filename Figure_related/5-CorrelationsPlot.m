955e254733647936af6a4583efd64ac1cc1eb96a
tchen0515 <157824281+tchen0515@users.noreply.github.com>
2024-07-04 16:20:26 -0400

    Update and rename CorrelationsPlot.m to 5-CorrelationsPlot.m

--- Figure_related/5-CorrelationsPlot.m
+++ Figure_related/5-CorrelationsPlot.m
@@ -3,12 +3,14 @@
 clearvars
 close all
 
-addpath '../../../../../../../../Misc Oceanography/Matlab Functions/'
-addpath 'C:\Users\Tz-Chian Chen\OneDrive - Florida State University\matlab&linux\djangraw\FormatFigures'
+% addpath '../../../../../../../../Matlab Functions/'
+% addpath '...\djangraw\FormatFigures'
 
 % import data (make sure which form is used. 'Reborn' for original anomaly, and 'std0410'for std anomaly)
-Intensity = readtable('C:\Users\Tz-Chian Chen\OneDrive - Florida State University\CalCOFI\Output\output_mhwbio\statistical_results\Statistics_MHWbio_Ori0423.xlsx','Sheet','OriIntensity_south');
-Duration = readtable('C:\Users\Tz-Chian Chen\OneDrive - Florida State University\CalCOFI\Output\output_mhwbio\statistical_results\Statistics_MHWbio_Ori0423.xlsx','Sheet','OriDuration_south');
+cd('C:\Users\Tz-Chian Chen\OneDrive - Florida State University\maunscript\MHW-bio\submission materials\Final data & code\')
+Intensity=readtable('Statistics_MHWbio_final.xlsx','UseExcel',true,'Sheet','OriIntensity_south',VariableNamingRule='preserve');
+Duration=readtable('Statistics_MHWbio_final.xlsx','UseExcel',true,'Sheet','OriDuration_south',VariableNamingRule='preserve');
+
 
 Duration.Labels = Duration.Index;
 Duration.Labels(find(strcmp(Duration.Index,'absChlalow1um'))) = {'Chl <1um'};
@@ -28,18 +30,20 @@ Duration.Labels(find(strcmp(Duration.Index,'Sardine-larvae'))) = {'Sardine Larva
 Duration.Labels(find(strcmp(Duration.Index,'Anchovy-larvae'))) = {'Anchovy Larvae'};
 
 % select the targeted variables
+
 phy_inds = [1:5,11:13,17:20,21,23,24];
 zoop_inds = 25:35;
 fish_inds = 36:39;
+nitra_inds = 40;
 
-
-fighandle = figure('pos',[10000 10000 10000 10000]);
+fighandle = figure('pos',[1000 1000 1000 1000]);
 fighandle.Units = 'inches';
 fighandle.Position = [1 1 3.5 3.5];
 hold on
-plot(abs(Intensity.Rho(phy_inds)),abs(Duration.Rho(phy_inds)),'ok','MarkerFaceColor',[0.5 0.5 1],'MarkerSize',5)
-plot(abs(Intensity.Rho(zoop_inds)),abs(Duration.Rho(zoop_inds)),'ok','MarkerFaceColor',[1 0.5 0.5],'MarkerSize',5)
-plot(abs(Intensity.Rho(fish_inds)),abs(Duration.Rho(fish_inds)),'ok','MarkerFaceColor',[0.5 1 0.5],'MarkerSize',5) 
+plot(abs(Intensity.Rho(phy_inds)),abs(Duration.Rho(phy_inds)),'ok','MarkerFaceColor',[0 0 1],'MarkerSize',5)
+plot(abs(Intensity.Rho(zoop_inds)),abs(Duration.Rho(zoop_inds)),'ok','MarkerFaceColor',[1 0 0],'MarkerSize',5)
+plot(abs(Intensity.Rho(fish_inds)),abs(Duration.Rho(fish_inds)),'ok','MarkerFaceColor',[0 1 0],'MarkerSize',5) 
+plot(abs(Intensity.Rho(nitra_inds)),abs(Duration.Rho(nitra_inds)),'ok','MarkerFaceColor',[1 1 0],'MarkerSize',5)
 significant = union(find(Duration.pval<0.05),find(Intensity.pval<0.05));
 phy_inds = intersect(significant,phy_inds);
 zoop_inds = intersect(significant,zoop_inds);
@@ -71,10 +75,6 @@ for i=1:length(phy_inds)  % locate the text labels
     end
 end
 for i=1:length(zoop_inds)
-%     if i==1
-%         text(abs(Intensity.Rho(zoop_inds(i)))-0.02,abs(Duration.Rho(zoop_inds(i)))+0.015,Duration.Labels(zoop_inds(i)),'FontSize',7)
-%     elseif i==length(zoop_inds)-1
-%          text(abs(Intensity.Rho(zoop_inds(i)))-0.04,abs(Duration.Rho(zoop_inds(i)))+0.015,Duration.Labels(zoop_inds(i)),'FontSize',7)
     if i==length(zoop_inds)-1
          text(abs(Intensity.Rho(zoop_inds(i)))-0.01,abs(Duration.Rho(zoop_inds(i)))+0.015,Duration.Labels(zoop_inds(i)),'FontSize',11)
     else
@@ -92,16 +92,12 @@ for i=1:length(fish_inds) % fish
     end
 
 end
-xlabel('Abs Correlation with SD-Intensity','FontSize',14)
-ylabel('Abs Correlation with Duration','FontSize',14)
+xlabel('Correlation with SD-Intensity (Abs value)','FontSize',14)
+ylabel('Correlation with Duration (Abs value)','FontSize',14)
 set(gca,'box','on','Fontsize',14)
-h=legend({'Microbes (n.s.)';'Zooplankton (n.s.)';'Fish (n.s.)';'Intensity (sig)';'Duration (sig)';'Both characteristics (sig)'},'Location','Best')
+h=legend({'Microbes';'Zooplankton';'Fish';'Nitracline';'Intensity (sig)';'Duration (sig)';'Both characteristics (sig)'},'Location','Best')
 ylim([0 0.4])
 xlim([0 0.4])
-% MakeLegend2([0.19 0.345; 0.005 0.08],['o','o','o','o','o','o'],flipud([0.7 0.7 1; 1 0.7 0.7; 0 0 1; 1 0 0]),...
-%     flipud({'Microbes (n.s.)';'Zooplankton (n.s.)';'Fish (sig)';'Microbes (sig)';'Metazoan Zooplankton (sig)';'Fish (sig)'}),fliplr([5 5 10 10]),8 )
 fn = 'CorrelationsPlot'
 exportgraphics(gcf,['',fn,'.png'],'Resolution',600)
 title('MHW characteristics correlation - suggestion')
-
-%set(h,'FontSize',8)
