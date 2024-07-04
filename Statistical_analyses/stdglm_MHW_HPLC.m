d98103d4262797e6886fa4e36f581772289424ba
tchen0515 <157824281+tchen0515@users.noreply.github.com>
2024-07-04 15:54:11 -0400

    Update stdglm_MHW_HPLC.m

--- Statistical_analyses/stdglm_MHW_HPLC.m
+++ Statistical_analyses/stdglm_MHW_HPLC.m
@@ -3,18 +3,17 @@
 close all
 clear all
 
-cd('C:\Users\Tz-Chian Chen\OneDrive - Florida State University\CalCOFI\Output\output_mhwbio\OriBio-SDMHW\')
-puremhw = readtable('OriFinal_MHW_HPLC.csv',VariableNamingRule='preserve'); %change file name
-% puremhwratio = readtable('Final_MHW_HPLC_ratio.csv',VariableNamingRule='preserve'); %change file name
-
+cd('...') % your directory
+mashup=readtable('MHW-in situ data.xlsx','UseExcel',true,'Sheet','Data Table (1)');
+mashup.DivinylChla=str2double(mashup.DivinylChla);
+mashup.Fucoxanthin=str2double(mashup.Fucoxanthin);
+mashup.Hexanoyloxyfucoxanthin=str2double(mashup.Hexanoyloxyfucoxanthin);
+puremhw=mashup;
 
 % extract variables
-hplc1=puremhw.dvChla(isnan(puremhw.dvChla)==0); %normal
-hplc2=puremhw.fucox(isnan(puremhw.fucox)==0); %normal
-hplc3=puremhw.hexfucox(isnan(puremhw.hexfucox)==0); %normal
-% hplc4=puremhwratio.dvChla(isnan(puremhwratio.dvChla)==0);  %normal 
-% hplc5=puremhwratio.fucox(isnan(puremhwratio.fucox)==0);  %normal
-% hplc6=puremhwratio.hexfucox(isnan(puremhwratio.hexfucox)==0);  %normal
+hplc1=puremhw.DivinylChla(isnan(puremhw.DivinylChla)==0); %normal
+hplc2=puremhw.Fucoxanthin(isnan(puremhw.Fucoxanthin)==0); %normal
+hplc3=puremhw.Hexanoyloxyfucoxanthin(isnan(puremhw.Hexanoyloxyfucoxanthin)==0); %normal
 
 
 % Check the data distribution (package:fitmethis)
@@ -25,17 +24,11 @@ hplc3=puremhw.hexfucox(isnan(puremhw.hexfucox)==0); %normal
 % Main statistical analysis part (maunally change the variable name)
 for i=1:3
 if i==1    %make sure the y indexes are corresponding
-     intensity = puremhw.anoSST(isnan(puremhw.dvChla)==0); % gev
-     duration = puremhw.rlduration(isnan(puremhw.dvChla)==0); % nbin
+     intensity = puremhw.anoSST(isnan(puremhw.DivinylChla)==0); % gev
+     duration = puremhw.rlduration(isnan(puremhw.DivinylChla)==0); % nbin
 elseif i==2||i==3
-     intensity = puremhw.anoSST; % gev
-     duration = puremhw.rlduration; % nbin
-% elseif i==4 
-%      intensity = puremhwratio.anoSST(isnan(puremhwratio.dvChla)==0); % gev
-%      duration = puremhwratio.rlduration(isnan(puremhwratio.dvChla)==0); % nbin
-% else
-%      intensity = puremhwratio.anoSST; % gev
-%      duration = puremhwratio.rlduration; % nbin
+     intensity = puremhw.anoSST(isnan(puremhw.Fucoxanthin)==0); % gev
+     duration = puremhw.rlduration(isnan(puremhw.Fucoxanthin)==0); % nbin
 end
 eval(['ano=',sprintf('hplc%d',i)]);
 
@@ -62,19 +55,12 @@ para(i,:)=[height(ano),rho1,pval1,b1(1),b1(2),bintr1(1,:),bintr1(2,:),bintjm1(1,
 end
 
 para=array2table(para);
-variable=array2table(["DivinylChla";"Fucoxanthin";"Hexanoyloxyfucoxanthin"]);
-% "ratioDivinylChla";"ratioFucoxanthin";"ratioHexanoyloxyfucoxanthin"]);
+variable=array2table(["DivinylChla";"Fucoxanthin";"HexanoyloxyFucoxanthinanthin"]);
 finalpara=[variable para];
 finalpara.Properties.VariableNames=["Index","Size","int-Rho","int-pval","B_int","B_slope","CI_int_low_R","CI_int_up_R","CI_slo_low_R",...
     "CI_slo_up_R","CI_int_low_JM","CI_int_up_JM","CI_slo_low_JM","CI_slo_up_JM","dur-Rho","dur-pval","B2_int","B2_slope","CI2_int_low_R","CI2_int_up_R","CI2_slo_low_R",...
     "CI2_slo_up_R","CI2_int_low_JM","CI2_int_up_JM","CI2_slo_low_JM","CI2_slo_up_JM"]
 
 % export the table
-cd ('C:\Users\Tz-Chian Chen\OneDrive - Florida State University\CalCOFI\Output\output_mhwbio\statistical_results')
+cd ('...')
 writetable(finalpara,"Oriresult_HPLC.csv") 
-
-
-
-% regression coefficients & a matrix BINT of the given confidence intervals for B
-% b=[intercept slope]
-% bintr & bintjm= confidence limits of intercept & slopr computed by "Ricker" or "Jolicoeur and Mosimann" procedure
